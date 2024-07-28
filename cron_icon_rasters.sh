89ed92092510a73f55558bd00a2076ac442b6c3f
Norman E. Avila <61478833+navilaufm@users.noreply.github.com>
2024-07-28 18:02:18 -0500

    agregado un cron

---
+++ cron_icon_rasters.sh
@@ -0,0 +1,11 @@
+echo $0
+cd "$(dirname "$0")"
+echo $(date)
+
+bash down_by_ext.sh https://cc1.meteo.tech/data/icon/ tif pcpn* /root/projects/pg_raster/down PCP
+bash down_by_ext.sh https://cc1.meteo.tech/data/icon/ tif temp* /root/projects/pg_raster/down TMP
+bash down_by_ext.sh https://cc1.meteo.tech/data/icon/ tif a_pcpn* /root/projects/pg_raster/down ACCP
+bash down_by_ext.sh https://cc1.meteo.tech/data/icon/ tif pcpn* /root/projects/pg_raster/down WSPD
+bash down_by_ext.sh https://cc1.meteo.tech/data/icon/ tif max_temp* /root/projects/pg_raster/down TMAX
+bash down_by_ext.sh https://cc1.meteo.tech/data/icon/ tif min_temp* /root/projects/pg_raster/down TMIN
+
