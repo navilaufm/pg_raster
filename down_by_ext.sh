4faaf0a548aec4aafcbe6a93fbf6f683e1aa793d
Norman E. Avila <61478833+navilaufm@users.noreply.github.com>
2024-07-28 13:47:44 -0500

    revert

--- down_by_ext.sh
+++ down_by_ext.sh
@@ -1,45 +1,21 @@
-#!/bin/bash
-
 # Check for required arguments
-if [ $# -ne 4 ]; then
-  echo "Usage: $0 <url> <extension> <pattern> <download_dir>"
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 <url> <extension> <download_dir>"
   exit 1
 fi
 
 # Assign arguments to variables
 url="$1"
 extension="$2"
-pattern="$3"
-download_dir="$4"
-
-# postgis
-database_name="rasters"
-srid=4326
+download_dir="$3"
 
 # Create download directory if it doesn't exist
 mkdir -p "$download_dir"
 
-# Fetch HTML content
-html=$(curl -s "$url")
+# Fetch HTML content, extract TIF file URLs, and download each file
+curl -s "$url" | grep -o 'href="[^"]*.'"$extension"'"' | sed 's/href="//' | sed 's/"$//' | xargs -I {} curl -O "$url"{}
 
-# Extract file URLs matching the extension
-files=$(echo "$html" | grep -o 'href="[^"]*'"$extension"'"' | sed 's/href="//' | sed 's/"$//')
-
-# Download each file
-for file in $files; do
-  filename=$(basename "$file")
-  
-  # Check if filename matches the pattern
-  if [[ "$filename" == $pattern ]]; then
-    wget -q "$url$file" -P "$download_dir"
-    echo "Downloaded: $download_dir/$filename"
-    variable=$(echo "$filename" | cut -d'_' -f1)
-    date=$(echo "$filename" | cut -d'_' -f2 | sed 's/\([0-9][0-9]\)\([0-9][0-9]\)\([0-9][0-9]\)/\1-\2-\3/')
-    echo "Filename: $filename  Variable: $variable Date: $date";
-    ##raster2pgsql -s "$srid" -d "$database_name" -t public.raster_data -I -C -a -F -W "filename='$filename';variable='$variable';date='$date'" "$file" | psql -d "$database_name"
-  else
-    echo "Skipping: $filename (does not match pattern)"
-  fi
-done
+# Move downloaded files to the specified directory
+mv *."$extension" "$download_dir"
 
 echo "Download completed."
