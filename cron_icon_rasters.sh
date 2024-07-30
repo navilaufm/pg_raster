0eb9225152127918892ef6f36d4cd567257bf9ca
Norman E. Avila <61478833+navilaufm@users.noreply.github.com>
2024-07-30 12:10:34 -0500

    Typo en Windspeed

--- cron_icon_rasters.sh
+++ cron_icon_rasters.sh
@@ -5,7 +5,7 @@ echo $(date)
 bash down_by_ext.sh https://cc1.meteo.tech/data/icon/ tif pcpn* /root/projects/pg_raster/down PCP
 bash down_by_ext.sh https://cc1.meteo.tech/data/icon/ tif temp* /root/projects/pg_raster/down TMP
 bash down_by_ext.sh https://cc1.meteo.tech/data/icon/ tif a_pcpn* /root/projects/pg_raster/down ACCP
-bash down_by_ext.sh https://cc1.meteo.tech/data/icon/ tif pcpn* /root/projects/pg_raster/down WSPD
+bash down_by_ext.sh https://cc1.meteo.tech/data/icon/ tif wspd* /root/projects/pg_raster/down WSPD
 bash down_by_ext.sh https://cc1.meteo.tech/data/icon/ tif max_temp* /root/projects/pg_raster/down TMAX
 bash down_by_ext.sh https://cc1.meteo.tech/data/icon/ tif min_temp* /root/projects/pg_raster/down TMIN
 
