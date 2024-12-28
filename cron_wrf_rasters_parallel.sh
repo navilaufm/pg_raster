985dfc3048d24145cd1b035fa47326846284813a
Norman E. Avila <61478833+navilaufm@users.noreply.github.com>
2024-12-28 13:04:42 -0600

    attempt con chatgpr

--- cron_wrf_rasters_parallel.sh
+++ cron_wrf_rasters_parallel.sh
@@ -1,35 +1,27 @@
-#!/bin/bash
+echo $0
+cd "$(dirname "$0")"
+echo $(date)INICIO
 
-# Define the base URL
-base_url="https://m.meteo.tech/w/"
-
-# Define the file extension
-file_ext="tif"
-
-# Define the output directory
-output_dir="/root/projects/pg_raster/down"
-
-# Define an array of file prefixes to download
-file_prefixes=(
-  "AFWA_CLOUD"
-  "AFWA_PWAT"
-  "AFWA_TOTPRECIP"
-  "CLDFRA"
-  "REFD_MAX"
-  "T2"
-  "TSK"
-  "WDIR10"
-  "WSPD10"
-  "WSPD10MAX"
-  "VIS"
-  "HR2"
-  "RAINC"
-  "RAINNC"
-  "RAINP"
-  "RAINTOT"
+# Define an array of commands
+commands=(
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif AFWA_CLOUD* /root/projects/pg_raster/down CLOUD"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif AFWA_PWAT* /root/projects/pg_raster/down PWAT"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif AFWA_TOTPRECIP* /root/projects/pg_raster/down TOTPRECIP"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif CLDFRA* /root/projects/pg_raster/down CLDFRA"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif REFD_MAX* /root/projects/pg_raster/down REFDMAX"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif T2* /root/projects/pg_raster/down T2"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif TSK* /root/projects/pg_raster/down TSK"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif WDIR10* /root/projects/pg_raster/down WDIR10"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif WSPD10* /root/projects/pg_raster/down WSPD10"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif WSPD10MAX* /root/projects/pg_raster/down WSPD10MAX"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif VIS* /root/projects/pg_raster/down VIS"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif HR2* /root/projects/pg_raster/down HR2"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif RAINC* /root/projects/pg_raster/down RAINC"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif RAINNC* /root/projects/pg_raster/down RAINNC"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif RAINP* /root/projects/pg_raster/down RAINP"
+    "bash down_by_ext.sh https://m.meteo.tech/w/ tif RAINTOT* /root/projects/pg_raster/down RAINTOT"
 )
 
-# Use GNU parallel to download files concurrently
-parallel --no-notice bash down_by_ext.sh {base_url} {file_ext} {} ${output_dir} ::: "${file_prefixes[@]}"
-
-echo "All downloads finished!"
+# Use parallel to execute the commands
+printf "%s\n" "${commands[@]}" | parallel --jobs 4
+echo $(date)FIN
